041eadcee3a29acf9ad114115a472770108e5e8d
Antony Fagundez <jhonfagundez@gmail.com>
2021-04-11 22:05:04 -0300

    Layout inicial

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
+import { Box } from "@chakra-ui/layout";
+import { SimpleGrid } from "@chakra-ui/layout";
 import React from "react";
+
+import CardContainer from "./CardContainer";
 function App() {
-  return <div>Todo App</div>;
+  return (
+    <SimpleGrid backgroundColor="gray.200" height="full" minChildWidth="120px" spacing="30px">
+      <Box height="container" p="2">
+        <CardContainer />
+      </Box>
+      <Box height="container" p="2">
+        <CardContainer />
+      </Box>
+      <Box height="container" p="2">
+        <CardContainer />
+      </Box>
+    </SimpleGrid>
+  );
 }
 
 export default App;
---
+++ src/CardContainer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Box } from "@chakra-ui/layout";
+
+const CardContainer = () => {
+  return (
+    <Box bg="white" borderRadius="xl" borderWidth="2px" height="container.sm" p="2" shadow="md" />
+  );
+};
+
+CardContainer.propTypes = {};
+
+export default CardContainer;
